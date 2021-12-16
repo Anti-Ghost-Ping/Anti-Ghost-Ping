83c3da48ec5e98017c2cca4466619164c78b0452
Leastrio <jacob@leastr.io>
2021-12-16 09:09:19 -0700

    Create file structuring

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::{env};
+use serenity::{
+    framework::{StandardFramework},
+    prelude::*,
+    async_trait,
+    model::{gateway::Ready}
+};
+use tracing::{error, info};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, _: Ready) {
+        info!("Connected");
+    }
+
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("Failed to load .env file");
+    
+    tracing_subscriber::fmt::init();
+
+    let token = env::var("BOT_TOKEN").expect("Could not find BOT_TOKEN in the environment");
+
+    let framework = StandardFramework::new().configure(|c| c.prefix("."));
+
+    let mut client = Client::builder(&token)
+        .framework(framework)
+        .event_handler(Handler)
+        .await
+        .expect("Error creating client");
+    {
+        let mut data = client.data.write().await;
+    }
+
+    if let Err(why) = client.start().await {
+        error!("Error Starting Client: {:?}", why);
+    }
+}
