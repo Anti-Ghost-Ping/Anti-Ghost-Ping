4c539f8183a63a6491583d2b51e5daaeea8b5b29
Leastrio <jacob@leastr.io>
2022-04-13 14:35:27 -0600

    Clean up database helper

--- src/helpers/database.rs
+++ src/helpers/database.rs
@@ -1,1 +1,10 @@
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
+pub async fn db_connect(
+    connection_string: &str,
+) -> Result<PgPool, Box<dyn std::error::Error + Send + Sync>> {
+    Ok(PgPoolOptions::new()
+        .max_connections(10)
+        .connect(connection_string)
+        .await?)
+}
