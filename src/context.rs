7b01becedb20a191cab1ad1fec101d2e582a40d8
Leastrio <jacob@leastr.io>
2022-04-13 14:34:42 -0600

    Add db pool to the context

--- src/context.rs
+++ src/context.rs
@@ -1,7 +1,9 @@
+use sqlx::PgPool;
 use twilight_cache_inmemory::InMemoryCache;
 use twilight_http::Client;
 
 pub struct AgpContext {
     pub http: Client,
     pub cache: InMemoryCache,
+    pub db: PgPool,
 }
